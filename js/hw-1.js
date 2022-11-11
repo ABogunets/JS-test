b393928c72cf21fb3b3930e05b6ef38eed69475c
ABogunets <abogunets@gmail.com>
2022-11-11 12:07:20 +0200

    hw-1 done upto Task13

---
+++ js/hw-1.js
@@ -0,0 +1,108 @@
+//HW-1
+
+// Task-1
+// const productName = "Droid";
+// console.log(productName);
+
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+
+// Task-2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// console.log(productName);
+
+// pricePerItem = pricePerItem + 1500;
+// console.log(pricePerItem);
+
+//Task-3
+// const topSpeed = 160;
+// const distance = 617.54;
+// let login = "mango935";
+// let isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed, distance, login, isOnline, isAdmin);
+
+// Task-4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// // Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+//Task-5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+//Task-6
+// let orderedQuantity;
+// orderedQuantity = 6;
+// let pricePerDroid;
+// pricePerDroid = 800;
+// let deliveryFee;
+// deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log("message", message);
+
+//Task-7
+// function sayHi() {
+//     console.log("Hello, this is my first function!");
+// };
+// sayHi();
+
+//Task-8
+// // Change code below this line
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+//   // Change code above this line
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+ 
+//Task-9
+// function add(a, b, c) {
+//   // Change code below this line
+//     return a + b + c;
+
+//   // Change code above this line
+// }
+// add(2, 5, 8); // 15
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+//Task-10
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
+// console.log(makeMessage('Apple', 20));
+
+//Task-11
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+//Task-12
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//     const message = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   // Change code above this line
+//   return message;
+// }
+// console.log("makeOrderMessage ~ message", makeOrderMessage(5, 4, 3));
+
+//Task-13
---
+++ js/module-1.js
@@ -0,0 +1,78 @@
+//Web1
+
+// const a = "Variable";
+// console.log(a);
+
+// let x;
+// x = 10;
+// console.log(x);
+
+// console.log("before");
+// alert ("Alert test")
+// console.log("After");
+
+// const shouldRenew = confirm("Do you want to renew your subscription?")
+// console.log(shouldRenew);
+
+// const quantity = prompt ("Enter the quantity of goods")
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let quantity = prompt("Enter the quantity of goods")
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = "50px";
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+// let elementWidth = "100px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log("elementWidth: ", elementWidth);
+
+// let elementWidth = "150.55";
+// elementWidth = Number.parseFloat(elementWidth);
+// console.log("elementWidth: ", elementWidth);
+
+// let salary = 1567.176583654;
+// salary = salary.toFixed(2);
+// salary = Number(salary);
+// console.log(salary);
+
+// let salary = 1567.176583654;
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
+
+// console.log(Number(salary.toFixed(2)));
+
+// let base = prompt("Give number");
+// base = Number(base);
+// console.log(base);
+// let power = prompt("Give power");
+// power = Number(power);
+// console.log(power);
+// const result = base ** power;
+// console.log(result);
+
+// const max = 80;
+// const min = 10;
+// const result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
+
+// const message = "happy birthday to you!";
+// console.log(message.length);
+
+// const quantity = 20;
+// const orderMsg = `You order ${quantity} fridges`;
+// console.log(orderMsg);
+
+// const brand = 'Samsung';
+// const normalizeBrand = brand.toLowerCase();
+// console.log(normalizeBrand);
+
+// console.log(5 && 6);
+// console.log(5 || 6);
+// console.log(!true);
+
+//Web2
