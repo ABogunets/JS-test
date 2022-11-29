4de3d78e324512871856778792bcc8c1f7242c8d
ABogunets <abogunets@gmail.com>
2022-11-30 00:07:58 +0200

    hw-3 upto30

--- js/hw-3.js
+++ js/hw-3.js
@@ -193,3 +193,332 @@
 // console.log("values", values);
 
 //Task-11
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   };
+//   // Change code above this line
+// }
+// console.log("keys", keys);
+// console.log("values", values);
+
+// Task-12
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     };
+//   };
+//     // Change code above this line
+//     return propCount;
+//   }
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// Task-13
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// };
+// console.log("keys", keys);
+// console.log("values", values);
+
+// Task-14
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   propCount = Object.keys(object).length;
+
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// Task-15
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log("keys", keys);
+// const values = Object.values(apartment);
+// console.log("values", values);
+
+// * Task-16
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const keys = Object.keys(salaries);
+//   for (const key of keys) {
+//     totalSalary += salaries[key];
+//   };
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// * another variant:
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   for (const value of values) {
+//     totalSalary += value;
+//   };
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// * Task-17
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// };
+// console.log("hexColors", hexColors);
+// console.log("rgbColors", rgbColors);
+
+// * Task-18
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price
+//     };
+//   };
+//   return null;
+//   // Change code above this line
+// }
+// console.log(getProductPrice('Droid'));
+  
+// * Task-19
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const propNameValues = [];
+//   for (const product of products) {
+//     if (Object.keys(product).includes(propName)) {
+//       propNameValues.push(product[propName]);
+//     };
+//   };
+//       return propNameValues;
+//     // Change code above this line
+// }
+//   console.log(getAllPropValues('name'));
+
+// * Task-20
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   let totalPriceOfProductName = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       totalPriceOfProductName += product.price * product.quantity;
+//     };
+//   };
+//   return totalPriceOfProductName;
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Grip"));
+
+// * Task-21
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+
+// // Change code below this line
+
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log("meanTemperature", meanTemperature);
+
+// * Task-22
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log("meanTemperature", meanTemperature);
+
+// * Task-23
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// // const highYesterday = highTemperatures.yesterday;
+// // const highToday = highTemperatures.today;
+// // const highTomorrow = highTemperatures.tomorrow;
+// // const highIcon = highTemperatures.icon;
+
+// const { yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+// console.log("meanTemperature", meanTemperature);
+
+// * Task-24
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const {hex, rgb} of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+// console.log("hexColors", hexColors);
+// console.log("rgbColors", rgbColors);
+
+// * Task-25
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const { today: { low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" }, tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } } = forecast;
+
+// * Task-26
+// Change code below this line
+// function calculateMeanTemperature(forecast) {
+
+//   const { today: { low: todayLow, high: todayHigh }, tomorrow: {low: tomorrowLow, high: tomorrowHigh} } = forecast;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(calculateMeanTemperature({
+//   today: { low: 10, high: 20 },
+//   tomorrow: { low: 20, high: 30 }
+// }));
+
+// * Task-27
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// console.log("bestScore", bestScore);
+// const worstScore = Math.min(...scores);
+// console.log("worstScore", worstScore);
+
+// * Task-28
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// console.log("allScores", allScores);
+// const bestScore = Math.max(...allScores);
+// console.log("bestScore", bestScore);
+// const worstScore = Math.min(...allScores);
+// console.log("worstScore", worstScore);
+
+// Task - 29
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = {...defaultSettings, ...overrideSettings};
+// console.log("finalSettings", finalSettings);
+
+// Task-30
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+//   // Change code below this line
+//   const madeTask = {completed, category, priority, ...data};
+//   return madeTask;
+//   // Change code above this line
+// }
+// console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }));
+
+// Task-31
